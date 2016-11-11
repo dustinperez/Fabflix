8f297e6b57e6a2d39e213e926fcbcd28bf441a89
Kyle Bendickson <kbendick@uci.edu>
2016-11-11 13:41:29 -0800

    Simplify the return type of getSchema

--- WEB-INF/src/models/DBMetaDataDAO.java
+++ WEB-INF/src/models/DBMetaDataDAO.java
@@ -23,17 +23,14 @@ public class DBMetaDataDAO {
 
 
     /**
-     * Returns a list of maps.
-     * Each map is a table in the database and its value
-     * is a list of all the columns found in that table, where
-     * each column in the table maps to its SQL datatype as a string.
+     * Maps a table name to a mapping of the column names in that table to their respective types.
      */
-    public static Map<String, List<Map<String, String>>> getSchema() {
+    public static Map<String, Map<String, String>> getSchema() {
 
         Connection connection = null;
         ResultSet tables = null;
         ResultSet columns = null;
-        Map<String, List<Map<String, String>>> schemaMap = new HashMap<String, List<Map<String, String>>>();
+        Map<String, Map<String, String>> schemaMap = new HashMap<String, Map<String, String>>();
 
         try {
 
@@ -42,17 +39,15 @@ public class DBMetaDataDAO {
             // Get all the tables in the database.
             tables = connection.getMetaData().getTables(null, null, null, null);
             while (tables.next()) {
-                List<Map<String, String>> columnsInTable = new ArrayList<Map<String, String>>();
-                schemaMap.put(tables.getString("TABLE_NAME"), columnsInTable);
+                schemaMap.put(tables.getString("TABLE_NAME"), new HashMap<String, String>>());
             }
 
             // Get all of the columns for each found table.
-            for (Map.Entry<String, List<Map<String, String>>> tableToColumns : schemaMap.entrySet()) {
-                columns = connection.getMetaData().getColumns(null, null, tableToColumns.getKey(), null);
-                while (columns.next()) {
-                    Map<String, String> columnToDataTypeMap = new HashMap<String, String>();
-                    columnToDataTypeMap.put(columns.getString("COLUMN_NAME"), columns.getString("TYPE_NAME"));
-                    tableToColumns.getValue().add(columnToDataTypeMap);
+            for (String table : schemaMap.keySet()) {
+                Map<String, String> tableColumns = schemaMap.get(table);
+                columnMetaData = connection.getMetaData().getColumns(null, null, tableToColumns.getKey(), null);
+                while (columnMetaData.next()) {
+                    tableColumns.put(columnMetaData.getString("COLUMN_NAME"), columnMetaData.getString("TYPE_NAME"));
                 }
             }
 
@@ -63,18 +58,17 @@ public class DBMetaDataDAO {
         } finally {
             try {
                 tables.close();
-            } catch (SQLException e) { }
+            } catch (SQLException e) { } // No worries.
             try {
                 columns.close();
-            } catch (SQLException e) { }
+            } catch (SQLException e) { } // No worries.
         }
     }
 
-    // TODO: Maybe make this print out in a nice format?
     public static void printDBSchema() {
         System.out.println();
-        Map<String, List<Map<String, String>>> dbSchema = DBMetaDataDAO.getSchema();
-        for (Map.Entry<String, List<Map<String, String>>> tableSchema : dbSchema.entrySet()) {
+        Map<String, Map<String, String>> dbSchema = DBMetaDataDAO.getSchema();
+        for (Map.Entry<String, Map<String, String>> tableSchema : dbSchema.entrySet()) {
             System.out.println("Table: " + tableSchema.getKey());
             System.out.println("Columns: " + tableSchema.getValue().toString());
             System.out.println();
