692be651657c791c6b49f4ebe174cf42b49c6732
Kyle Bendickson <kbendick@uci.edu>
2016-11-11 13:43:30 -0800

    Formatting changes

--- WEB-INF/src/models/DBSchema.java
+++ WEB-INF/src/models/DBSchema.java
@@ -12,18 +12,15 @@ public class DBSchema {
 
     /* Inner class for Table */
     public static class Table {
-
         public String name;
         public List<String> columns;
-
         public Table() {
             this.name = null;
             this.columns = new ArrayList<String>();
         }
-
         public Table(String name) {
+            this()
             this.name = name;
-            this.columns = new ArrayList<String>();
         }
 
         public List<String> getColumns() { return this.columns; }
@@ -49,20 +46,4 @@ public class DBSchema {
     }
 
     public List<Table> getTableList() { return this.tableList; }
-
-//    public static DBSchema buildDBSchema(Map<String, List<Map<String, String>>> schemaMap) {
-//
-//        DBSchema dbSchema = new DBSchema();
-//        for (Map.Entry<String, List<Map<String, String>>> tableSchema : schemaMap.entrySet()) {
-//
-//            String tableName = tableSchema.getKey();
-//            System.out.println("Table: " + tableName);
-//            System.out.println("Columns: " + tableSchema.getValue().toString());
-//            System.out.println();
-//
-//            dbSchema.addTable(tableSchema.getKey());
-//        }
-//
-//        return null;
-//    }
 }
